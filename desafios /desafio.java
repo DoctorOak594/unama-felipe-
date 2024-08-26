385cc4b14f01a0e677c42b610f556434473cbbed
DoctorOak594 <mikaluiggi@gmail.com>
2024-08-26 13:27:44 +0000

    resolvemos o desafio 01

---
+++ desafios /desafio.java
@@ -0,0 +1,15 @@
+importjava.util.Scanner;
+public class desafio1{
+    public static void main(string[]args) {
+        scanner leitura=new scanner(system.im);
+        system.out.println("digite um numero");
+        int numero=leitura.nextint();
+        system.out.println;
+        for(int i=1;i<=numero;i++){
+            for(int j=1;j<=i; j++){
+                system.out.print(j+"");
+            }
+            system.out.println("");
+            
+
+
